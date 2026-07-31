f3e8efdc8dffad93bcb16ecca3a10e258a442616
dogeystamp <dogeystamp@disroot.org>
2026-07-30 21:56:26 -0400

    timer: refactor computed amounts

--- timer/src/main.rs
+++ timer/src/main.rs
@@ -3,7 +3,7 @@ use std::{
     time::Duration,
 };
 
-use chrono::Local;
+use chrono::{Local, TimeDelta};
 use clap::{Parser, Subcommand};
 use serde::{Deserialize, Serialize};
 
@@ -25,6 +25,17 @@ enum Session {
     Stopped,
 }
 
+/// Computed [`Session`] information.
+enum SessionStatus {
+    Ongoing {
+        duration: Duration,
+        start_time: chrono::DateTime<Local>,
+        end_time: chrono::DateTime<Local>,
+        until_end: TimeDelta,
+    },
+    Stopped,
+}
+
 impl Session {
     /// Side-effectful update
     fn update(self) -> Self {
@@ -43,6 +54,25 @@ impl Session {
                 }
             }
             Session::Stopped => self,
+        }
+    }
+
+    fn status(&self) -> SessionStatus {
+        match &self {
+            Session::Ongoing {
+                start_time,
+                duration,
+            } => {
+                let end_time = *start_time + *duration;
+                let now = chrono::offset::Local::now();
+                SessionStatus::Ongoing {
+                    duration: *duration,
+                    start_time: *start_time,
+                    end_time,
+                    until_end: end_time - now,
+                }
+            }
+            Session::Stopped => SessionStatus::Stopped,
         }
     }
 }
@@ -72,11 +102,13 @@ enum Command {
     /// Start break timer
     Start {
         /// Duration in minutes
-        #[arg(default_value = "0")]
+        #[arg(default_value = "60")]
         duration: u64,
     },
     /// Get break/task status
     Status {},
+    /// Get short status
+    Short {},
 }
 
 /// Trigger finishing actions.
@@ -111,6 +143,7 @@ fn main() {
     };
 
     let mut state = read_state(&state_file).unwrap_or_default();
+    state.current_session = state.current_session.update();
 
     let cli = Cli::parse();
     match cli.command {
@@ -122,23 +155,35 @@ fn main() {
             }
         }
         Command::Status {} => {
-            state.current_session = state.current_session.update();
-            match state.current_session {
-                Session::Ongoing {
-                    start_time,
+            println!("SESSION");
+            match state.current_session.status() {
+                SessionStatus::Ongoing {
+                    until_end,
                     duration,
+                    start_time,
+                    end_time,
                 } => {
-                    let end = start_time + duration;
-                    let now = chrono::offset::Local::now();
-                    let delta = end - now;
-                    let secs = delta.num_seconds();
-                    println!("session: {:>02}:{:>02} remaining", secs / 60, secs % 60,);
+                    let secs = until_end.num_seconds();
+                    println!(
+                        "{} -> {} ({}m)",
+                        start_time.format("%H:%m"),
+                        end_time.format("%H:%m"),
+                        duration.as_secs() / 60
+                    );
+                    println!("{:>02}:{:>02} remaining", secs / 60, secs % 60,);
                 }
-                Session::Stopped => {
-                    println!("session: no tracking")
+                SessionStatus::Stopped => {
+                    println!("no tracking")
                 }
             }
         }
+        Command::Short {} => match state.current_session.status() {
+            SessionStatus::Ongoing { until_end, .. } => {
+                let secs = until_end.num_seconds();
+                println!("{:>02}:{:>02}", secs / 60, secs % 60,);
+            }
+            SessionStatus::Stopped => println!("--:--"),
+        },
     }
 
     if cli.verbose {
